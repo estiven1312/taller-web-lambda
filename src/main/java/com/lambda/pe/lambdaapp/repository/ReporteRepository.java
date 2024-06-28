3ad9665e59a7fb1af037c457306fc755b5926788
Estiven Salvador Hurtado Santos <santosestiven46@gmail.com>
2024-06-28 01:27:51 -0500

    Add message

--- src/main/java/com/lambda/pe/lambdaapp/repository/ReporteRepository.java
+++ src/main/java/com/lambda/pe/lambdaapp/repository/ReporteRepository.java
@@ -3,5 +3,7 @@ package com.lambda.pe.lambdaapp.repository;
 import com.lambda.pe.lambdaapp.domain.model.Reporte;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+@Repository
 public interface ReporteRepository extends JpaRepository<Reporte, Long> {
 }
