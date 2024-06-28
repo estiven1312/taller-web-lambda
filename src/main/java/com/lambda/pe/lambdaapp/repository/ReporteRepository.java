a95ad6b22d5be8777b01fd761b8cc185483eb350
Estiven Salvador Hurtado Santos <santosestiven46@gmail.com>
2024-06-27 23:38:11 -0500

    Fix validation of files in report garage

--- src/main/java/com/lambda/pe/lambdaapp/repository/ReporteRepository.java
+++ src/main/java/com/lambda/pe/lambdaapp/repository/ReporteRepository.java
@@ -2,6 +2,8 @@ package com.lambda.pe.lambdaapp.repository;
 
 import com.lambda.pe.lambdaapp.domain.model.Reporte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReporteRepository extends JpaRepository<Reporte, Long> {
 }
